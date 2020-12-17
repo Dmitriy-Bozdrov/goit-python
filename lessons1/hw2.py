eea8126b5175de7357022eb44e3214daa19f42f3
Dmitriy-Bozdrov <bozdrov@gmail.com>
2020-12-17 22:49:32 +0200

    hw1

---
+++ lessons1/hw2.py
@@ -0,0 +1,17 @@
+﻿print("Введите коэффициенты для уравнения")
+print("ax**2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+D = b ** 2 - 4 * a * c
+print("Дискриминант D : ",+ float(D))
+
+
+x1 = (-b + D**0.5) / 2*a
+
+x2 = (-b - D**0.5) / 2*a
+
+
+print("x1 = ", float(x1))
+print("x2 = ", float(x2))
