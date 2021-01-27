1635104f4ebeaccda229489af5f0e6482eff88ad
Dmitriy <76182183+Dmitriy-Bozdrov@users.noreply.github.com>
2021-01-27 22:50:08 +0200

    Update hw3.py

--- lessons2/hw3.py
+++ lessons2/hw3.py
@@ -1,12 +1,12 @@
-﻿result = 0
-operator = ' '
-while operator != '=':
+result = 0
+operator = None
+    while operator != '=':
     try:
         number = float(input('Введите число: '))
     except ValueError:
         print('Error, try again')
         continue
-    if operator == None:
+    if not operator:
         result = number
     elif operator == '+':
         result += number
@@ -15,16 +15,16 @@ while operator != '=':
     elif operator == "*":
         result *= number
     elif operator == "/":
-        try:
-            result /= number
-        except ZeroDivisionError:
-            print("Нельзя делить на ноль")
-            continue
+    try:
+        result /= number
+    except ZeroDivisionError:
+        print("Нельзя делить на ноль")
+        continue
     operator = input('Введите оператор: ')
-    while operator not in ('+-*/='):
+    while operator not in '+-*/=':
         print('Error, try again1')
         operator = input('Введите оператор: ')
     if operator == '=':
-        continue   
-               
+        continue
+
 print(result)
