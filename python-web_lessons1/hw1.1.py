f95e598aa137c8bd3bbee52de326dfa3f6ab0f19
Dmitriy <76182183+Dmitriy-Bozdrov@users.noreply.github.com>
2022-05-20 21:14:59 +0300

    Update hw1.1.py

--- python-web_lessons1/hw1.1.py
+++ python-web_lessons1/hw1.1.py
@@ -4,25 +4,38 @@ import pickle
 
 
 class SerializationInterface(ABC):
-    
+
+    @property
     @abstractmethod
-    def create_json_file(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
-    def create_bin_file(self, *args, **kwargs):
-        pass
-
-
-class Serialization(SerializationInterface):
+    def some_data(self):
+        return some_data
     
-    def create_json_file(self, *args, **kwargs): 
-        with open("data_file.json", "w") as file:
-            json.dump(args, file)
             
-    def create_bin_file(self, *args, **kwargs):
+class SerializationJson(SerializationInterface):
+
+    some_data = {'key': 'value', 2: [1, 2, 3], 'tuple': (5, 6), 'a': {'key': 'value'}}
+
+    def create_json_file(self): 
+        with open("data_file.json", "w") as file:
+            json.dump(self.some_data, file)
+
+    
+class SerializationBin(SerializationInterface):
+    
+    some_data = {'key': 'value', 2: [1, 2, 3], 'tuple': (5, 6), 'a': {'key': 'value'}}
+
+    def create_bin_file(self):
         with open("data_file.bin", "wb") as file:
-            pickle.dump(args, file)     
+            pickle.dump(self.some_data, file)
+           
+
+dser_json = SerializationJson()
+dser_json.create_json_file()
+
+dser_bin = SerializationBin()
+dser_bin.create_bin_file()
+
+
 
 
 
